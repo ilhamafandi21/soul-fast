9763d11b1333b80cc0cb24b3697424e4e9186852
ilham <ilhamafandi21@gmail.com>
2025-11-04 22:34:49 +0700

    04 Nov 2025

---
+++ lib/pages/main/main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:soulfast/pages/home/home_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
+  }
+}
