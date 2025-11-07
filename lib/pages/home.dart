d125980819f8e5fdb4fa9b496f758630b9181df5
ilham <ilhamafandi21@gmail.com>
2025-11-07 21:25:55 +0700

    07 Nov 2025

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,55 +9,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  String? selectedFast;
-  int durationToSecond = 0;
-  int remainingSecond = 0;
-  Timer? countdownTimer;
-  List<String> variantFast = [
-    '16/8',
-    '18/6',
-    '24 Jam',
-    '36 Jam',
-    '48 Jam',
-    '72 Jam',
-  ];
+ 
 
-  void timerSet() {
-    switch (selectedFast) {
-      case '16/8':
-        durationToSecond = 16 * 3600;
-        break;
-      case '18/6':
-        durationToSecond = 18 * 3600;
-        break;
-      case '24 Jam':
-        durationToSecond = 24 * 3600;
-        break;
-      case '36 Jam':
-        durationToSecond = 36 * 3600;
-        break;
-      case '48 Jam':
-        durationToSecond = 48 * 3600;
-        break;
-      case '72 Jam':
-        durationToSecond = 72 * 3600;
-        break;
-      default:
-        durationToSecond = 0 * 3600;
-    }
-  }
 
-  void startFasting() {
-    countdownTimer = Timer.periodic(Duration(seconds: 1), (timer) {
-      if (durationToSecond > 0) {
-        setState(() {
-          durationToSecond--;
-        });
-      } else {
-        timer.cancel();
-      }
-    });
-  }
 
   String formatTime(int remainingSecond) {
     final hours = remainingSecond ~/ 3600;
@@ -72,60 +26,23 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('SoulFast!'),
+        title: Text('SoulFast'),
         backgroundColor: Colors.blue,
-        titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
       ),
       body: Container(
-        height: 200,
-        width: 200,
-        margin: EdgeInsets.all(5),
+        width: double.infinity,
         decoration: BoxDecoration(
-          color: Colors.cyan,
-          borderRadius: BorderRadius.all(Radius.circular(20)),
+          color: Colors.blue[300],
         ),
         child: Column(
-          spacing: 10,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Container(
-              height: 30,
+              height: 100,
               width: 100,
               decoration: BoxDecoration(
-                color: Colors.cyan[100],
-                borderRadius: BorderRadius.circular(20),
+                color: Colors.amber[300],
               ),
-              child: DropdownButton(
-                underline: SizedBox(),
-                padding: EdgeInsets.only(left: 10),
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                dropdownColor: Colors.cyan[300],
-                value: selectedFast,
-                hint: Text('Select', style: TextStyle(fontSize: 14)),
-                items: variantFast.map((value) {
-                  return DropdownMenuItem(
-                    value: value,
-                    child: Text(value, style: TextStyle(fontSize: 14)),
-                  );
-                }).toList(),
-                onChanged: (e) {
-                  setState(() {
-                    selectedFast = e;
-                    timerSet();
-                  });
-                },
-              ),
-            ),
-            Text(durationToSecond.toString() ),
-            ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: WidgetStatePropertyAll(Colors.cyan[100]),
-              ),
-              onPressed: () {
-                startFasting();
-              },
-              child: Text('Mulai'),
-            ),
+            )
           ],
         ),
       ),
