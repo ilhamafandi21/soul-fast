1fb0ee8debd9c53def1ef57165ff23c3e02424aa
ilham <ilhamafandi21@gmail.com>
2025-11-09 00:39:34 +0700

    11  nov  2025

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,7 +10,16 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
- 
+  String? selectedFast;
+  List<String> variantFast = [
+    '16/8',
+    '18/6',
+    '24 Jam',
+    '36 Jam',
+    '48 Jam',
+    '72 Jam',
+  ];
+
   String formatTime(int remainingSecond) {
     final hours = remainingSecond ~/ 3600;
     final minutes = (remainingSecond % 3600) ~/ 60;
@@ -23,7 +32,28 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(title: Text('SoulFast!'), backgroundColor: Colors.amber),
+      body: Container(
+        child: Column(
+          children: [
+            DropdownButton<String>(
+              value: selectedFast,
+              hint: Text('Select'),
+              items: variantFast.map((value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value.toString()),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedFast = value.toString();
+                });
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
