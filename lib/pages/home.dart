5b261b8c035751f1b0930905ab0d6a8e7487b767
ilham <ilhamafandi21@gmail.com>
2025-11-08 08:41:51 +0700

    08-NOV-2025

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -95,7 +95,7 @@ class _HomeState extends State<Home> {
               child: Column(
                 children: [
                   dropDownFasting(),
-                  Text(formatTime(duration).toString()),
+                  Text(formatTime(duration).()),
                   ElevatedButton(
                     onPressed: () {
                       if (countdownFasting != null &&
