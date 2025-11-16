044d9ec9cb48c2d59202913f061bc053208061e6
ilham <ilhamafandi21@gmail.com>
2025-11-16 21:20:09 +0700

    16 -nov- 2025

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -142,15 +142,7 @@ class _HomeState extends State<Home> {
                           },
                           child: Text(
                             (countdownTimer != null && countdownTimer!.isActive)
-                                ? 'Stop'
-                                : 'Start',
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+          
             ),
           ),
         ),
