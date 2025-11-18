0f6bc73ff09eb63be214eb26db3709d620a55ec6
ilham <ilhamafandi21@gmail.com>
2025-11-19 00:26:47 +0700

    19 -nov- 2025

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -27,7 +27,11 @@ class _HomeState extends State<Home> {
   void durationFasting() {
     switch (selectedFasting) {
       case '5 Detik':
-        duration =
+        duration = 5;
+        break;
+      case '16/8':
+        duration = 16 * 3600;
+        break;
       case '18/6':
         duration = 18 * 3600;
         break;
