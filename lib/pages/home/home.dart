05e7f80db2d2a42b51e67d258ef00a66b2fe7bc5
ilham <ilhamafandi21@gmail.com>
2025-11-24 22:13:47 +0700

    24 -nov -2025

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -8,6 +8,57 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String? selectedVariant;
+  int durationFasting = 0;
+  List<DropdownMenuItem<String>> get variantFasting {
+    List<String> variants = [
+      '16/8',
+      '18/6',
+      '20/4',
+      '24 hours',
+      '36 hours',
+      '48 hours',
+    ];
+    return variants
+        .map(
+          (variant) => DropdownMenuItem(value: variant, child: Text(variant)),
+        )
+        .toList();
+  }
+
+  void updateDurationFasting(String? variant) {
+    switch (variant) {
+      case '16/8':
+        durationFasting = 16 * 3600;
+        break;
+      case '18/6':
+        durationFasting = 18 * 3600;
+        break;
+      case '20/4':
+        durationFasting = 20 * 3600;
+        break;
+      case '24 hours':
+        durationFasting = 24 * 3600;
+        break;
+      case '36 hours':
+        durationFasting = 36 * 3600;
+        break;
+      case '48 hours':
+        durationFasting = 48 * 3600;
+        break;
+      default:
+        durationFasting = 0;
+    }
+  }
+
+  String formatTime(int remainingSecond) {
+    final hours = remainingSecond ~/ 3600;
+    final minutes = (remainingSecond % 3600) ~/ 60;
+    final seconds = remainingSecond % 60;
+    // Format dengan 2 digit, misal 01:05:09
+    return '${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,10 +71,44 @@ class _HomeState extends State<Home> {
           child: Column(
             children: [
               Container(
-                height: 100,
+                height: 50,
                 width: double.infinity,
-                margin: EdgeInsets.all(4),
-                decoration: BoxDecoration(color: Colors.blueAccent),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 151, 201, 242),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 10,
+                  children: [
+                    DropdownButton<String>(
+                      hint: Text('Select'),
+                      value: selectedVariant,
+                      items: variantFasting,
+                      onChanged: (value) {
+                        setState(() {
+                          selectedVariant = value;
+                          updateDurationFasting(value);
+                        });
+                      },
+                    ),
+                    ElevatedButton(onPressed: () {}, child: Text('Start')),
+                  ],
+                ),
+              ),
+              Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 151, 201, 242),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Duration: ${formatTime(durationFasting).toString()}'),
+                  ],
+                ),
               ),
             ],
           ),
