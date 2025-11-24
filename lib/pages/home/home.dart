ec65054b9d0cff0b9badc1cb524af117840aff00
ilham <ilhamafandi21@gmail.com>
2025-11-25 04:53:23 +0700

    25 -11- 2025

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -51,7 +51,16 @@ class _HomeState extends State<Home> {
         durationFasting = 36 * 3600;
         break;
       case '48 hours':
-        durationF/ 60;
+        durationFasting = 48 * 3600;
+        break;
+      default:
+        durationFasting = 0;
+    }
+  }
+
+  String formatTime(int remainingSecond) {
+    final hours = remainingSecond ~/ 3600;
+    final minutes = (remainingSecond % 3600) ~/ 60;
     final seconds = remainingSecond % 60;
     // Format dengan 2 digit, misal 01:05:09
     return '${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
