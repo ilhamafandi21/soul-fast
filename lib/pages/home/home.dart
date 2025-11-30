6c0b663a5007991b5265a88f4877e933dc136621
ilham <ilhamafandi21@gmail.com>
2025-11-30 22:12:53 +0700

    30 -nov- 2025

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -4,7 +4,53 @@ import 'package:flutter/material.dart';
 
 import 'package:shared_preferences/shared_preferences.dart';
 
-class H
-              Elev
+class Home exten
+      case '18/6':
+        durationFasting = 18 * 3600;
+        break;
+      case '20/4':
+        durationFasting = 20 * 3600;
+        break;
+      case '24 hours':
+        durationFasting = 24 * 3600;
+        break;');
+    final selectFasting = prefs.getString('selected_fasting');
+    final hasil = endTimeMillis! - currentTime;
+
+    print('Selected Fasring: $selectFasting');
+    print('Current Time: $currentTime');
+    print('Finish Time: $endTimeMillis');
+    print('Hasil: $hasil');
+  }
+
+  void stopFasting() {
+    countdow
+                    stopFasting();
+                    selectedFasting = e;
+                    duration();
+                  });
                 },
-          
+              ),
+
+              ElevatedButton(
+                onPressed: () {
+                   saveFastingDuration(
+                    durationFasting,
+                    selectedFasting.toString(),
+                  );
+                  startFasting();
+                 
+                },
+                child: Text('Start'),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text('Duration Fasting: $durationFasting')],
+          ),
+        ],
+      ),
+    );
+  }
+}
