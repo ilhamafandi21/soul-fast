34837d284753ea842bf34e75582267ed374d4d49
ilham <ilhamafandi21@gmail.com>
2025-11-30 22:12:36 +0700

    30 -nov- 2025

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -5,11 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Home exten
-      case
-    print('Finish Time: $endTimeMillis');
-    print('Hasil: $hasil');
-  }
-
   void stopFasting
               Elev
                 },
