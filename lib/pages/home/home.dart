13977e8294126aaf5738b588d98274f99abf39a4
ilham <ilhamafandi21@gmail.com>
2025-12-01 00:21:07 +0700

    01 -dec- 2025

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -5,16 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Home exten
-      case '18/6':
-        durationFasting = 18 * 3600;
-        break;
-      case '20/4':
-        durationFasting = 20 * 3600;
-        break;
-      case '24 hours':
-        durationFasting = 24 * 3600;
-        break;');
-    final selectFasting = prefs.getString('selected_fasting');
+      case '18/6':lected_fasting');
     final hasil = endTimeMillis! - currentTime;
 
     print('Selected Fasring: $selectFasting');
