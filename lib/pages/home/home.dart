d3d7af6979e12e2d4584a9eb285b82cf7d775ed1
ilham <ilhamafandi21@gmail.com>
2025-12-06 17:01:13 +0700

    06 12 2025

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -28,6 +28,14 @@ class _HomeState extends State<Home> {
       final now = DateTime.now().millisecondsSinceEpoch;
       final diff = endTime - now;
 
+
+      setState(() {
+        if (diff <= 0) {
+          timer?.cancel();
+        } else {
+          remainingTime = diff ~/ 1000;
+        }
+      });
     });
   }
 
