2635c1686788b8d3348ccce825e7eb97f7193d2a
ilham <ilhamafandi21@gmail.com>
2025-12-07 01:10:57 +0700

    07 12 2025

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -18,7 +18,10 @@ class _HomeState extends State<Home> {
 
   void startFasting() async {
     // Save endTime
-    final prefs = await SharedPreferences.getInstance
+    final prefs = await SharedPreferences.getInstance();
+    final endTime = DateTime.now().add(Duration(seconds: duration + 2)).millisecondsSinceEpoch;
+    prefs.setInt('endTime', endTime);
+
 
     timer = Timer.periodic(Duration(seconds: 1), (e) {
 
