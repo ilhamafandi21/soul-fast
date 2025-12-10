122101b6b494f6d3438f60998800652430a72e51
ilham <ilhamafandi21@gmail.com>
2025-12-10 21:51:46 +0700

    10 12 2025

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -9,30 +9,56 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int durationFasting = 0;
 
-
-
+  void duration() {
+    switch (selectedFasting) {
+      case "5 Detik":
+        durationFasting = 5;
+        break;
+      case "16/8":
+        durationFasting = 16 * 3600;
+        break;
+      case "18/6":
+        durationFasting = 18 * 3600;
+        break;
+      case "20/4":
+        durationFasting = 20 * 3600;
+        break;
+      case "24 Jam":
+        durationFasting = 24 * 3600;
+        break;
+      default:
+        durationFasting = 0;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('App Fasting'), backgroundColor: Colors.amber),
-      body: SafeArea(child: SingleChildScrollView(
-        child: Column(
-          children: [
-            DropdownButton(
-              value: selectedFasting,
-              hint: Text('Pilih Jenis Fasting'),
-              items: variantFasting, onChanged: (e) {
-                setState(() {
-                  selectedFasting = e;
-                });
-              }),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              DropdownButton(
+                value: selectedFasting,
+                hint: Text('Pilih Jenis Fasting'),
+                items: variantFasting,
+                onChanged: (e) {
+                  setState(() {
+                    selectedFasting = e;
+                    duration();
+                  });
+                },
+              ),
 
-              ElevatedButton(onPressed: (){}, child: Text('Mulai Fasting')),
-          ],
+              ElevatedButton(onPressed: () {}, child: Text('Mulai Fasting')),
+              Text(durationFasting.toString()),
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
