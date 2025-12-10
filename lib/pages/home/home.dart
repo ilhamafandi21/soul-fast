5ee91513394082cd980cf317065bee7c3dbb0130
ilham <ilhamafandi21@gmail.com>
2025-12-10 21:41:41 +0700

    10 12 2025

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,7 +1,4 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:soulfast/pages/home/data.dart';
 
 class Home extends StatefulWidget {
@@ -12,73 +9,30 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  Timer? timer;
-  int remainingTime = 0;
-  int endTime = 0;
-
-  void startFasting() async {
-    // Save endTime
-    final prefs = await SharedPreferences.getInstance();
-    final endTime = DateTime.now().add(Duration(seconds: duration + 2)).millisecondsSinceEpoch;
-    prefs.setInt('endTime', endTime);
 
 
-    timer = Timer.periodic(Duration(seconds: 1), (e) {
 
-      final now = DateTime.now().millisecondsSinceEpoch;
-      final diff = endTime - now;
-
-
-      setState(() {
-        if (diff <= 0) {
-          timer?.cancel();
-        } else {
-          remainingTime = diff ~/ 1000;
-        }
-      });
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Fasting App')),
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Column(
-            children: [
-              DropdownButton(
-                value: selectedFast,
-                hint: Text('Select Fasting Type'),
-                items: variantFast,
-                onChanged: (e) {
-                  setState(() {
-                    selectedFast = e;
-                    totalDuration();
-                  });
-                },
-              ),
-              Text('Selected Fast: $selectedFast'),
-              Text('Total Duration (seconds): $duration'),
-              Text(
-                formatTime(duration).toString(),
-                style: TextStyle(fontSize: 30),
-              ),
+      appBar: AppBar(title: Text('App Fasting'), backgroundColor: Colors.amber),
+      body: SafeArea(child: SingleChildScrollView(
+        child: Column(
+          children: [
+            DropdownButton(
+              value: selectedFasting,
+              hint: Text('Pilih Jenis Fasting'),
+              items: variantFasting, onChanged: (e) {
+                setState(() {
+                  selectedFasting = e;
+                });
+              }),
 
-              Text(remainingTime.toString(), style: TextStyle(fontSize: 30)),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll(Colors.blue[200]),
-                ),
-                onPressed: () {
-                  startFasting();
-                },
-                child: Text('Start Fast'),
-              ),
-            ],
-          ),
+              ElevatedButton(onPressed: (){}, child: Text('Mulai Fasting')),
+          ],
         ),
-      ),
+      )),
     );
   }
 }
