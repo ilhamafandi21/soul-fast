5f7ebc867e7f5a729c38dc8323d673473c8b55ff
ilham <ilhamafandi21@gmail.com>
2025-12-13 04:36:24 +0700

    13 12 2025

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -67,12 +67,7 @@ class _HomeState extends State<Home> {
                 Container(
                   height: 140,
                   width: 200,
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 228, 255, 121),
-                    borderRadius: BorderRadius.circular(50)
-                  ),
-                  child: Column(
-                    children: [
+                  decoration: BoxD
                       DropdownButton(
                   value: selectedFasting,
                   hint: Text('Pilih Jenis Fasting'),
