54b2860fba35d8c262b08391dbafdde46cb1d275
ilham <ilhamafandi21@gmail.com>
2025-12-12 21:25:41 +0700

    12 12 2025

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -87,14 +87,6 @@ class _HomeState extends State<Home> {
                 ),
             
                 ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      startFasting();
-                    });
-                  },
-                  child: Text('Mulai Fasting'),
-                ),
-                Text(
                   (timer != null && timer!.isActive)
                       ? formatTime(remainingTime)
                       : formatTime(durationFasting),
