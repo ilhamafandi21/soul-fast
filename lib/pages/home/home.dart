8a2a9111d5de0c0fdc65bf168ae57c3446ee1fc7
ilham <ilhamafandi21@gmail.com>
2025-12-15 05:10:49 +0700

    15 12 2015

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -13,7 +13,12 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('
+      appBar: AppBar(title: Text('App Fasting!')),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              DropdownButton(
                 hint: Text('Select'),
                 value: selectedFasting,
                 items: variantFasting,
