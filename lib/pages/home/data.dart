37aec19df98c28114b9064a7f336c64a1ad57962
ilham <ilhamafandi21@gmail.com>
2025-12-14 21:44:53 +0700

    14 12 2015

--- lib/pages/home/data.dart
+++ lib/pages/home/data.dart
@@ -1,41 +1,29 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 
 int durationFasting = 0;
-String? selectedFasting;
-
-List<DropdownMenuItem<String>> get variantFasting {
-  List<String> fastingType = ["5 Detik", "16/8", "18/6", "20/4", "24 Jam"];
-  return fastingType.map((e) {
-    return DropdownMenuItem(value: e, child: Text(e));
-  }).toList();
-}
-
-void duration() {
-  switch (selectedFasting) {
-    case "5 Detik":
-      durationFasting = 5;
-      break;
-    case "16/8":
-      durationFasting = 16 * 3600;
-      break;
-    case "18/6":
-      durationFasting = 18 * 3600;
-      break;
-    case "20/4":
-      durationFasting = 20 * 3600;
-      break;
-    case "24 Jam":
-      durationFasting = 24 * 3600;
-      break;
-    default:
-      durationFasting = 0;
+  String? selectedFasting;
+  List<DropdownMenuItem<String>> get variantFasting {
+    List<String> fastingType = ['5 Detik', '16/8', '18/6', '24 Jam'];
+    return fastingType.map((e) {
+      return DropdownMenuItem(value: e, child: Text(e));
+    }).toList();
   }
-}
 
-String formatTime(int remainingSecond) {
-  final hours = remainingSecond ~/ 3600;
-  final minutes = (remainingSecond % 3600) ~/ 60;
-  final seconds = remainingSecond % 60;
-  // Format dengan 2 digit, misal 01:05:09
-  return '${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
-}
+  void duration() {
+    switch (selectedFasting) {
+      case '5 Detik':
+        durationFasting = 5;
+        break;
+      case '16/8':
+        durationFasting = 16 * 3600;
+        break;
+      case '18/6':
+        durationFasting = 18 * 3600;
+        break;
+      case '24 Jam':
+        durationFasting = 24 * 3600;
+        break;
+      default:
+        durationFasting = 0;
+    }
+  }
--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -9,34 +9,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int durationFasting = 0;
-  String? selectedFasting;
-  List<DropdownMenuItem<String>> get variantFasting {
-    List<String> fastingType = ['5 Detik', '16/8', '18/6', '24 Jam'];
-    return fastingType.map((e) {
-      return DropdownMenuItem(value: e, child: Text(e));
-    }).toList();
-  }
-
-  void duration() {
-    switch (selectedFasting) {
-      case '5 Detik':
-        durationFasting = 5;
-        break;
-      case '16/8':
-        durationFasting = 16 * 3600;
-        break;
-      case '18/6':
-        durationFasting = 18 * 3600;
-        break;
-      case '24 Jam':
-        durationFasting = 24 * 3600;
-        break;
-      default:
-        durationFasting = 0;
-    }
-  }
-
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
