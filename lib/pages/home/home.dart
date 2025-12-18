eb5d66a23a423df7bfbdbefb3180a197e6080307
ilham <ilhamafandi21@gmail.com>
2025-12-18 21:45:09 +0700

    18 dec 2025

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -18,30 +18,6 @@ class _HomeState extends State<Home> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              DropdownButton(
-                hint: Text('Select'),
-                value: selectedFasting,
-                items: variantFasting,
-                onChanged: (e) {
-                  setState(() {
-                    selectedFasting = e;
-                    duration();
-                  });
-                },
-              ),
-
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll(
-                    const Color.fromARGB(255, 2, 255, 78),
-                  ),
-                ),
-                onPressed: () {},
-                child: Text('Mulai Fasting'),
-              ),
-
-              Text(durationFasting.toString()),
-            ],
           ),
         ),
       ),
