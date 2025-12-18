2e07170420590fe60f342d029a83b8d17816da2b
ilham <ilhamafandi21@gmail.com>
2025-12-19 05:01:55 +0700

    19 12 2025

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -6,39 +6,7 @@ class Home extends StatefulWidget {
 
   @override
   State<Home> createState() => _HomeState();
-}
 
-class _HomeState extends State<Home> {
- 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('App Fasting!')),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              DropdownButton(
-                hint: Text('Select'),
-                value: selectedFasting,
-                items: variantFasting,
-                onChanged: (e) {
-                  setState(() {
-                    selectedFasting = e;
-                    duration();
-                  });
-                },
-              ),
-
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll(
-                    const Color.fromARGB(255, 2, 255, 78),
-                  ),
-                ),
-                onPressed: () {},
-                child: Text('Mulai Fasting'),
-              ),
 
               Text(durationFasting.toString()),
             ],
