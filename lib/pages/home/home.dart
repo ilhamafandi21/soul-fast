127075019efcb825bf7fed1de2414cf98ca8f0f1
ilham <ilhamafandi21@gmail.com>
2025-12-22 21:27:47 +0700

    22 12 2025

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -19,12 +19,7 @@ class _HomeState extends State<Home> {
           child: Column(
             children: [
               DropdownButton(
-                hint: Text('Select'),
-                value: selectedFasting,
-                items: variantFasting,
-                onChanged: (e) {
-                  setState(() {
-                    selectedFasting = e;
+                hint: Text(' e;
                     duration();
                   });
                 },
