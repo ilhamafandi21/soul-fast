ed478bd802e12eaf03e6d86ee14f44ffc739b13f
ilham <ilhamafandi21@gmail.com>
2025-12-24 22:04:20 +0700

    24 12 2025

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -5,9 +5,6 @@ class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
-}
-
 class _HomeState extends State<Home> {
  
   @override
