846b74916ccdf8b14259b5542bf026980c54525c
ilham <ilhamafandi21@gmail.com>
2025-12-26 21:35:55 +0700

    26 12 2025

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -17,16 +17,6 @@ class _HomeState extends State<Home> {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
-            children: [
-              DropdownButton(
-                hint: Text('Select'),
-                value: selectedFasting,
-                items: variantFasting,
-                onChanged: (e) {
-                  setState(() {
-                    selectedFasting = e;
-                    duration();
-                  });
                 },
               ),
 
