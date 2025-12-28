19c92c61b203282699acb6cb55310a15732b618e
ilham <ilhamafandi21@gmail.com>
2025-12-28 22:29:00 +0700

    28 12 2025

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -3,11 +3,6 @@ import 'package:soulfast/pages/home/data.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
 class _HomeState extends State<Home> {
  
   @override
