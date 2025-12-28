dddbde5faf16fe5e32a864f71968a799e44bbcc3
ilham <ilhamafandi21@gmail.com>
2025-12-29 02:47:10 +0700

    29 12 2025

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -5,6 +5,12 @@ class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+ 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('App Fasting!')),
