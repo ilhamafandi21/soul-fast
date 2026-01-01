cdce08e4d340c07e453e4fc01408d23975537441
ilham <ilhamafandi21@gmail.com>
2026-01-01 21:30:24 +0700

    01 01 2026

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -11,6 +11,10 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
  
   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('App Fasting!')),
+      body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
             children: [
