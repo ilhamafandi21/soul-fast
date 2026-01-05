07adb6e276324c72ef7051cae92cfc7467fb26d0
ilham <ilhamafandi21@gmail.com>
2026-01-06 00:54:55 +0700

    06 JAN 2026

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -27,6 +27,11 @@ class _HomeState extends State<Home> {
                     selectedFasting = e;
                     duration();
                   });
+                },
+              ),
+
+              ElevatedButton(
+                style: ButtonStyle(
                   backgroundColor: WidgetStatePropertyAll(
                     const Color.fromARGB(255, 2, 255, 78),
                   ),
