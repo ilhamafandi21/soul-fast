fb401e75ed4bdb9d3c8203e8b587c791a0c7e10d
ilham <ilhamafandi21@gmail.com>
2026-01-05 23:32:09 +0700

    05 JAN 2026

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -37,6 +37,7 @@ class _HomeState extends State<Home> {
                   ),
                 ),
                 onPressed: () {},
+                child: Text('Mulai Fasting'),
               ),
 
               Text(durationFasting.toString()),
