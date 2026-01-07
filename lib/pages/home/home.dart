0ad71ff2226df7c40247a187a5ba2642a777a88b
ilham <ilhamafandi21@gmail.com>
2026-01-07 21:19:25 +0700

    08012026

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -35,12 +35,7 @@ class _HomeState extends State<Home> {
                   backgroundColor: WidgetStatePropertyAll(
                     const Color.fromARGB(255, 2, 255, 78),
                   ),
-                ),
-                onPressed: () {},
-                child: Text('Mulai Fasting'),
-              ),
-
-              Text(durationFasting.toString()),
+                ),()),
             ],
           ),
         ),
