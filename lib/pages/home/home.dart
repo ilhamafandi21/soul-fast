3da9e16dc41cf69e5559e2260f0c806b443338f5
ilham <ilhamafandi21@gmail.com>
2026-01-10 00:39:17 +0700

    10 01 2026

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -29,10 +29,7 @@ class _HomeState extends State<Home> {
                   });
                 },
               ),
-
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll(
+ertyAll(
                     const Color.fromARGB(255, 2, 255, 78),
                   ),
                 ),
