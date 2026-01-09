6694d6a8dded0c8e780d8d4db46d3d387bd837f1
ilham <ilhamafandi21@gmail.com>
2026-01-09 21:28:50 +0700

    09 JAN 2026

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -36,9 +36,7 @@ class _HomeState extends State<Home> {
                     const Color.fromARGB(255, 2, 255, 78),
                   ),
                 ),
-                onPressed: () {},
-                child: Text('Mulai Fasting'),
-              ),
+                onPressed: () {}
 
               Text(durationFasting.toString()),
             ],
