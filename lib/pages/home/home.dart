29757f5641e5d49bed7b66d5feb9cdb95f17dbcc
ilham <ilhamafandi21@gmail.com>
2026-01-11 22:14:28 +0700

    11 01 2026

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -32,6 +32,9 @@ class _HomeState extends State<Home> {
 
               ElevatedButton(
                 style: ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(
+                    const Color.fromARGB(255, 2, 255, 78),
+                  ),
                 ),
                 onPressed: () {},
                 child: Text('Mulai Fasting'),
